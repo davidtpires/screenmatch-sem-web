66854ece4d1b2ed637b2a9588a342c1d7383be56
DavidT <davidt.info@gmail.com>
2024-10-16 19:25:13 -0300

    Um novo projeto utilizando o Spring Framework

--- src/main/java/br/com/alura/screammatch/ScreammatchApplication.java
+++ src/main/java/br/com/alura/screammatch/ScreammatchApplication.java
@@ -1,13 +1,18 @@
 package br.com.alura.screammatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreammatchApplication {
+public class ScreammatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreammatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro projeto Spring sem Web!");
+	}
 }
