4c4025dd3d92bd202e59d9f8f0ae8260656a88ee
DavidT <davidt.info@gmail.com>
2024-10-16 19:23:04 -0300

    first commit

---
+++ src/main/java/br/com/alura/screammatch/ScreammatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screammatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreammatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreammatchApplication.class, args);
+	}
+
+}
