7b5ff333ab9d5edf313101ace939bb7d293c9754
DavidT <davidt.info@gmail.com>
2024-10-17 20:34:49 -0300

    Trabalhando na coleção de dados

--- src/main/java/br/com/alura/screammatch/principal/Principal.java
+++ src/main/java/br/com/alura/screammatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screammatch.principal;
 
+import br.com.alura.screammatch.model.DadosEpisodio;
 import br.com.alura.screammatch.model.DadosSerie;
 import br.com.alura.screammatch.model.DadosTemporada;
 import br.com.alura.screammatch.service.ConsumoApi;
@@ -33,5 +34,15 @@ public class Principal {
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++ ){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
     }
 }
