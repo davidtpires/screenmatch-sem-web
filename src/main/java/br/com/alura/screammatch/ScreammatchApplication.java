f168bad0325342b7afccb741448fdc49e800616e
DavidT <davidt.info@gmail.com>
2024-10-17 19:27:25 -0300

    Modelando temporadas

--- src/main/java/br/com/alura/screammatch/ScreammatchApplication.java
+++ src/main/java/br/com/alura/screammatch/ScreammatchApplication.java
@@ -2,11 +2,15 @@ package br.com.alura.screammatch;
 
 import br.com.alura.screammatch.model.DadosEpisodio;
 import br.com.alura.screammatch.model.DadosSerie;
+import br.com.alura.screammatch.model.DadosTemporada;
 import br.com.alura.screammatch.service.ConsumoApi;
 import br.com.alura.screammatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreammatchApplication implements CommandLineRunner {
@@ -29,6 +33,16 @@ public class ScreammatchApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=4f942238");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season="+i+"&apikey=4f942238");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 
 }
