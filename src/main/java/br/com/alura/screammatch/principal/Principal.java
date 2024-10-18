c846b33dfeec2df7f004f5b2f22258194e8ffba5
David T. <davidt.info@gmail.com>
2024-10-18 14:29:11 -0300

    Identificando os top 5 melhores episódios

--- src/main/java/br/com/alura/screammatch/principal/Principal.java
+++ src/main/java/br/com/alura/screammatch/principal/Principal.java
@@ -6,10 +6,8 @@ import br.com.alura.screammatch.model.DadosTemporada;
 import br.com.alura.screammatch.service.ConsumoApi;
 import br.com.alura.screammatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -20,39 +18,43 @@ public class Principal {
     private final String API_KEY = "&apikey=4f942238";
 
     public void exibeMenu() {
-//        System.out.println("Digite o nome da série para buscar:");
-//        var nomeSerie = leitura.nextLine();;
-//        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ","+") + API_KEY);
-//        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-//        System.out.println(dados);
-//
-//        List<DadosTemporada> temporadas = new ArrayList<>();
-//
-//        for (int i = 1; i <= dados.totalTemporadas(); i++){
-//            json = consumo.obterDados(ENDERECO +
-//                    nomeSerie.replace(" ","+") + "&season="+ i + API_KEY);
-//            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-//            temporadas.add(dadosTemporada);
-//        }
-//        temporadas.forEach(System.out::println);
+        System.out.println("Digite o nome da série para buscar:");
+        var nomeSerie = leitura.nextLine();;
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ","+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
 
-//        for (int i = 0; i < dados.totalTemporadas(); i++) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for(int j = 0; j < episodiosTemporada.size(); j++ ){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
+        List<DadosTemporada> temporadas = new ArrayList<>();
 
-//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        for (int i = 1; i <= dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO +
+                    nomeSerie.replace(" ","+") + "&season="+ i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
 
-        List<String> nomes = Arrays.asList("Jaque", "Iasmin", "Paulo", "Rodrigo", "Nico");
+        for (int i = 0; i < dados.totalTemporadas(); i++) {
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++ ){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
 
-        nomes.stream()
-                .sorted()
-                .limit(3)
-                .filter(n -> n.startsWith("N"))
-                .map(n -> n.toUpperCase())
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+//        List<String> nomes = Arrays.asList("Jaque", "Iasmin", "Paulo", "Rodrigo", "Nico");
+//        nomes.stream().sorted().limit(3).filter(n -> n.startsWith("N")).map(n -> n.toUpperCase()).forEach(System.out::println);
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avalicao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avalicao).reversed())
+                .limit(5)
                 .forEach(System.out::println);
-
     }
 }
