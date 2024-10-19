5303509cf8996436b9e741a230d7f012dbd8af30
DavidT <davidt.info@gmail.com>
2024-10-19 10:11:58 -0300

    Coletando estatísticas

--- src/main/java/br/com/alura/screammatch/principal/Principal.java
+++ src/main/java/br/com/alura/screammatch/principal/Principal.java
@@ -107,5 +107,13 @@ public class Principal {
                         Collectors.averagingDouble(Episodio::getAvalicao)));
         System.out.println(avaliacoesPorTemporada);
 
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvalicao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvalicao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: "+ est.getCount());
+
     }
 }
